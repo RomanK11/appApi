ff25c089079e8dec52bd2ccf73140ec53cccf61f
Roman <romankorovin032@gmail.com>
2023-12-02 21:52:13 +0300

    first commit

---
+++ src/main/java/com/api/laps/LapsApplication.java
@@ -0,0 +1,13 @@
+package com.api.laps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/laps/LapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.laps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
